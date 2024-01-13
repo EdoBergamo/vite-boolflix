694e2887b17a8c9f322a28ea70eb1dd00c1d0405
Edo <hello@edoardo-hb.it>
2024-01-13 21:46:41 +0100

    feat: show series

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,6 +9,7 @@ export default {
       store,
       searchQuery: "",
       movies: [],
+      series: [],
     };
   },
 
@@ -30,10 +31,13 @@ export default {
   methods: {
     async searchMovies() {
       const movieUrl = `${this.store.endpointMovie}api_key=${this.apiKey}&query=${this.searchQuery}`;
+      const seriesUrl = `${this.store.endpointSeries}api_key=${this.apiKey}&query=${this.searchQuery}`;
 
-      axios
-        .get(movieUrl)
-        .then((response) => (this.movies = response.data.results));
+      const movieResponse = await axios.get(movieUrl);
+      this.movies = movieResponse.data.results;
+
+      const seriesResponse = await axios.get(seriesUrl);
+      this.series = seriesResponse.data.results;
     },
   },
 };
@@ -47,6 +51,7 @@ export default {
     </div>
 
     <ul class="movie-list" v-if="movies.length > 0">
+      <li>Movies</li>
       <li class="movie-card" v-for="movie in movies" :key="movie.id">
         <h2>{{ movie.title }}</h2>
         <p>Titolo: {{ movie.original_title }}</p>
@@ -55,6 +60,15 @@ export default {
           <img :src="getLanguageFlagUrl(movie.original_language)" alt="Flag" />
         </p>
         <p>Voto: {{ movie.vote_average }}</p>
+      </li>
+    </ul>
+
+    <ul class="series-list" v-if="series.length > 0">
+      <li>Series</li>
+      <li class="series-card" v-for="serie in series" :key="serie.id">
+        <h2>Titolo: {{ serie.original_name }}</h2>
+        <p>Lingua: {{ serie.original_language }}</p>
+        <p>Voto: {{ serie.vote_average }}</p>
       </li>
     </ul>
   </div>
